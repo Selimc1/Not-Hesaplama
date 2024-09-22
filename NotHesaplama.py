8349021f3758b39f7418d63203cfb890681778f9
Selimc1 <selimc2002@hotmail.com>
2024-09-22 19:29:13 +0300

    Create NotHesaplama.py

---
+++ NotHesaplama.py
@@ -0,0 +1,15 @@
+notu = int(input("Notunuzu girin: "))
+
+if notu >= 90:
+    print("Notunuz AA")
+elif notu >= 80:
+    print("Notunuz BA")
+elif notu >= 70:
+    print("Notunuz BB")
+elif notu >= 60:
+    print("Notunuz CB")
+else:
+    print("Dersten geçemediniz")
+
+
+input("Çıkmak için Enter tuşuna basın.")
